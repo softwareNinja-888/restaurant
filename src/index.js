f45ddffcedc2ecb73486b0bab277f6095d89a40f
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-08-24 23:19:10 +0200

    Add create home function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './style.css'
 import {displayImg,heading,information} from './home.js'
+import { head } from 'lodash'
 
 const createHome = ()=>{
-    
+    heading()
+    displayImg()
+    information()
 }
+createHome()
